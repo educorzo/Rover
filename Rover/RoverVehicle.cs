d832f3d441faddd1df89b61f07f58b1f863ec456
eduardo.corzo <edukorzo@gmail.com>
2022-03-07 13:46:04 +0100

    Boiler plate

---
+++ Rover/RoverVehicle.cs
@@ -0,0 +1,76 @@
+﻿namespace Rover;
+
+public class RoverVehicle
+{
+    private string _direction;
+    private int _x;
+    private int _y;
+
+    public RoverVehicle()
+    {
+        _direction = "N";
+    }
+
+    private void Move()
+    {
+        switch (_direction)
+        {
+            case "N":
+                _y++;
+                break;
+            case "W":
+                _x--;
+                break;
+            case "S":
+                _y--;
+                break;
+            case "E":
+                _x++;
+                break;
+        }
+    }
+    
+    public string Execute(string command)
+    {
+
+        foreach (var c in command.ToCharArray())
+        {
+            switch (c)
+            {
+                case 'L':
+                    _direction = RotateLeft();  
+                    continue;
+                case 'R':
+                    _direction = RotateRight();
+                    break;
+                case 'M':
+                    Move();
+                    break;
+            }
+        }
+        
+        return _x + ":" + _y + ":" + _direction;
+    }
+    
+    private string RotateLeft()
+    {
+        if (_direction == "N")
+            return "W";
+        if (_direction == "W")
+            return "S";
+        if (_direction == "S")
+            return "E";
+        return "N";
+    }
+    
+    private string RotateRight()
+    {
+        if (_direction == "N")
+            return "E";
+        if (_direction == "W")
+            return "N";
+        if (_direction == "S")
+            return "W";
+        return "S";
+    }
+}
---
+++ RoverTests/RoverTests.cs
@@ -0,0 +1,50 @@
+using Rover;
+using Xunit;
+
+namespace RoverTests;
+
+public class RoverShould
+{
+    [Theory]
+    [InlineData("L","0:0:W")]
+    [InlineData("LL","0:0:S")]
+    [InlineData("LLL","0:0:E")]
+    [InlineData("LLLL","0:0:N")]
+    public void RotateLeft(string command, string result)
+    {
+        var rover = new RoverVehicle();
+        
+        var status = rover.Execute(command);
+        
+        Assert.Equal(result, status);
+    }
+    
+    [Theory]
+    [InlineData("R","0:0:E")]
+    [InlineData("RR","0:0:S")]
+    [InlineData("RRR","0:0:W")]
+    [InlineData("RRRR","0:0:N")]
+    public void RotateRight(string command, string result)
+    {
+        var rover = new RoverVehicle();
+        
+        var status = rover.Execute(command);
+        
+        Assert.Equal(result, status);
+    }
+    
+    [Theory]
+    [InlineData("M","0:1:N")]
+    [InlineData("LM","-1:0:W")]
+    [InlineData("LLM","0:-1:S")]
+    [InlineData("RM","1:0:E")]
+    [InlineData("RMLMLMRMMLMR","-1:3:N")]
+    public void Moves(string command, string result)
+    {
+        var rover = new RoverVehicle();
+        
+        var status = rover.Execute(command);
+        
+        Assert.Equal(result, status);
+    }
+}
